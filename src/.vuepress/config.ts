a7fec008196037185ae24c5cb14253bdf747daf2
faga <lzc295592@163.com>
2023-09-18 18:12:44 +0800

    doc: add fontRender

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,63 +1,65 @@
-import { defaultTheme, defineUserConfig } from 'vuepress';
-import { hopeTheme } from 'vuepress-theme-hope';
+import { defaultTheme, defineUserConfig } from "vuepress";
+import { hopeTheme } from "vuepress-theme-hope";
 export default defineUserConfig({
-    theme: hopeTheme({
-      author: {
-        name: 'faga',
-        url: "http://www.faga.cc"
-      },
-      repoDisplay: false,
-      navbar: [
-        { text:'主页', link:'/' },
-        { text: '笔记', link: '/blog/tokei' },
-        { text: 'Github', link: 'https://github.com/faga1' }
+  theme: hopeTheme({
+    author: {
+      name: "faga",
+      url: "http://www.faga.cc",
+    },
+    repoDisplay: false,
+    navbar: [
+      { text: "主页", link: "/" },
+      { text: "笔记", link: "/blog/tokei" },
+      { text: "Github", link: "https://github.com/faga295" },
+    ],
+    sidebar: {
+      "/blog/": [
+        "fontRender",
+        "tokei",
+        "histoire",
+        "vite",
+        "remark",
+        "react",
+        "web_upload_oss",
+        "webpack",
+        "interview",
+        "commonjs&esmodule",
       ],
-      sidebar: {
-        '/blog/':[
-          'tokei',
-          'histoire',
-          'vite',
-          'remark',
-          'react',
-          'web_upload_oss',
-          'webpack',
-          'interview',
-          'commonjs&esmodule',
-        ]
+    },
+    blog: {
+      name: "faga",
+      avatar:
+        "https://lzc-personal-resource.oss-cn-beijing.aliyuncs.com/images/typora/微信图片_20220607163536.jpg",
+      medias: {
+        Github: "https://github.com/faga1",
       },
-      blog: {
-        name: 'faga',
-        avatar: 'https://lzc-personal-resource.oss-cn-beijing.aliyuncs.com/images/typora/微信图片_20220607163536.jpg',
-        medias: {
-          Github: 'https://github.com/faga1'
-        }
+    },
+    plugins: {
+      blog: true,
+      comment: {
+        provider: "Waline",
+        serverURL: "https://blog-comment-theta-six.vercel.app/",
       },
-      plugins: {
-        blog: true,
-        comment: {
-          provider: "Waline",
-          serverURL: "https://blog-comment-theta-six.vercel.app/"
-        },
 
-        pwa: {
-          manifest:{
-            name: "faga's blog",
-          }
-        }
-      }
-    }),
-    
-    title: "faga's blog",
-    description: 'Just playing around',
-    dest:'docs',
-    // theme: defaultTheme({
-    //   sidebarDepth:2,
-    //   // searchMaxSuggestions: 10,
-    //   navbar: [
-    //     { text:'主页', link:'/' },
-    //     { text: '笔记', link: '/blog/' },
-    //     { text: 'Github', link: 'https://github.com/faga1' }
-    //   ],
-    
-    // })
-})
+      pwa: {
+        manifest: {
+          name: "faga's blog",
+        },
+      },
+    },
+  }),
+
+  title: "faga's blog",
+  description: "Just playing around",
+  dest: "docs",
+  // theme: defaultTheme({
+  //   sidebarDepth:2,
+  //   // searchMaxSuggestions: 10,
+  //   navbar: [
+  //     { text:'主页', link:'/' },
+  //     { text: '笔记', link: '/blog/' },
+  //     { text: 'Github', link: 'https://github.com/faga1' }
+  //   ],
+
+  // })
+});
---
+++ src/blog/fontRender.md
@@ -0,0 +1,86 @@
+---
+title: 字体渲染 
+date: 2023-09-18
+tags: rust
+---
+
+# 字体渲染
+
+## 字体的种类
+
+目前最常见的字体都是矢量字体，在早期，文字的渲染都是通过点阵字体的形式
+
+点阵字体: 
+
+![](https://lzc-personal-resource.oss-cn-beijing.aliyuncs.com/202309181519685.png)
+
+点阵字体和矢量字体可以理解为位图和矢量图的区别，矢量字体是通过数学公式去确定的字体轨迹，因此它可以很方便的缩放，而点阵字体就是有一群像素点组成，它不能很方便的缩放，也不能很方便的旋转。目前最常见的矢量字体是ttf，当然你可能也听过woff，woff是在浏览器上字体渲染的优化，他相比较ttf体积减少了40%，当然它本质还是ttf。
+
+## 字体渲染的流程
+
+在渲染之前，我们需要拿到一个.ttf的字体，并把它加载进来，我们可以通过[font-kit](https://github.com/servo/font-kit)这种工具加载系统已经安装好的字体。拿到字体之后， 我们需要去解析这个字体，我们可以使用[ttf-parser](https://github.com/RazrFalcon/ttf-parser), 解析之后，我们就可以实现光栅化某一个字符，在文本渲染中，这些光栅化后的字符称之为字形(glyph).但是目前只能光栅化某一个字符，并不能光栅化一个字符串，因此我们就需要对字形布局(layout)，这个布局就是需要控制每一个字符和字符之间的位置，包括换行也是需要layout去做，[fontdue](https://github.com/mooman219/fontdue)是基于`ttf-parser`, 实现了光栅化和布局的功能，因此在加载完字体后就可以直接通过fontdue去做字体的渲染。
+
+## 使用fontdue渲染字符串
+
+想要通过fontdue渲染一个字符，非常的简单，README上面就有例子，但是fontdue并没有直接提供渲染字符串的例子，因为作者认为布局和每个字符的bitmap都给你了，那你自己就可以渲染(附上issue: https://github.com/mooman219/fontdue/issues/128)
+
+我们先学习一下README上提供的例子
+
+```rust
+// Read the font data.
+let font = include_bytes!("../resources/Roboto-Regular.ttf") as &[u8];
+// Parse it into the font type.
+let font = fontdue::Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
+// Rasterize and get the layout metrics for the letter 'g' at 17px.
+let (metrics, bitmap) = font.rasterize('g', 17.0);
+```
+
+由于fontdue只是个光栅化字体的工具，所以加载字体直接使用的`include_bytes`, 这会把这个ttf字体在编译时直接变编译成二进制的形式加入到产物中，你可以使用font-kit去代替include_bytes这一步。在`rasterize`(光栅化)后，我们可以得到metrics和bitmap，bitmap就是位图，metrics是用来描述渲染后字形的像素信息的![](https://lzc-personal-resource.oss-cn-beijing.aliyuncs.com/202309181515519.png)
+
+```rust
+let font = include_bytes!("../assets/Roboto-Regular.ttf") as &[u8];
+let roboto_regular = Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
+let fonts = &[roboto_regular];
+let mut layout = Layout::new(CoordinateSystem::PositiveYDown);
+layout.reset(&LayoutSettings {
+  ..LayoutSettings::default()
+});
+layout.append(fonts, &TextStyle::new(&self.text, self.font_size, 0));
+
+let mut glyphs:Vec<Vec<u8>> = vec![];
+// 光栅化渲染
+self.text.chars().for_each(|c| {
+  let (_, bitmap) = fonts[0].rasterize(c, self.font_size);
+  glyphs.push(bitmap);
+});
+// 获取整个字符串的宽高
+let dim= compute_dim(&layout);
+
+let mut bitmap:Vec<u8> = vec![0; dim.0 * dim.1];
+// 遍历每一个字形，将其渲染到合适的位置
+for (pos, char_bitmap) in zip(layout.glyphs(), &glyphs) {
+  let x = pos.x as i32;
+  let y = pos.y as i32 as i32;
+  let width = pos.width as usize;
+  let height = pos.height as usize;
+  let mut i = 0;
+  for y in y..y+height as i32 {
+    for x in x..x+width as i32 {
+      let index = ((y * dim.0 as i32 + x))  as usize;
+      if index < bitmap.len() {
+        bitmap[index] = char_bitmap[i];
+      }
+      i += 1;
+    }
+  }
+}
+
+```
+
+
+
+## 参考
+
+* [字体渲染 : 如何渲染一个文字](https://blog.yangteng.me/articles/2019/typography-how-fonts-are-rendered/)
+* [文字渲染](https://learnopengl-cn.readthedocs.io/zh/latest/06%20In%20Practice/02%20Text%20Rendering/)
+* [相关issue](https://github.com/RazrFalcon/tiny-skia/issues/1)
